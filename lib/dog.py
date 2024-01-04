00c453fe299267760c5228f95df09789d7670801
33-66 <simon.nganga@student.moringaschool.com>
2024-01-04 04:26:49 +0300

    CRUD operations completed

--- lib/dog.py
+++ lib/dog.py
@@ -1,22 +1,33 @@
 from models import Dog
 
-def create_table(base):
+def create_table(Base,engine):
+    Base.metadata.create_all(engine)
     pass
-
 def save(session, dog):
+    session.add(dog)
+    session.commit()
     pass
-
 def get_all(session):
+    query = session.query(Dog).all()
+    return query
     pass
 
 def find_by_name(session, name):
-    pass
+    """Return a list of Dogs with the given name"""
+    query = session.query(Dog).filter(Dog.name == name).first()
+    return query
+    
 
 def find_by_id(session, id):
+    query = session.query(Dog).filter(Dog.id == id) .first()
+    return query
     pass
-
 def find_by_name_and_breed(session, name, breed):
+    query = session.query(Dog).filter(Dog.breed == breed ,Dog.name == name ).first()
+    return query
     pass
 
 def update_breed(session, dog, breed):
+    dog.breed = breed
+    session.commit()
     pass
--- lib/models.py
+++ lib/models.py
@@ -2,12 +2,13 @@
 
 from sqlalchemy import (Column, String, Integer)
 from sqlalchemy.ext.declarative import declarative_base
-
+from sqlalchemy import create_engine
 Base = declarative_base()
-
+engine = create_engine('sqlite:///dogs.db')
 class Dog(Base):
     __tablename__ = 'dogs'
 
     id = Column(Integer(), primary_key=True)
     name = Column(String())
     breed = Column(String())
+
